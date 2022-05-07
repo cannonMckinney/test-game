b6b3d1f7352688c241c38bc9257da0d45aa891c5
cannonMckinney <cannon.mckinney@wildoradoisd.org>
2022-05-06 22:20:36 -0500

    css link test

--- main.js
+++
@@ -1,9 +0,0 @@
-function myFunction(par, strokes) {
-    if (strokes = 1) {
-        return "hole in one";
-         
-    }else if (strokes <= par - 2) {
-        return "eagle";
-    }
-}
-myFunction(1, 1);
